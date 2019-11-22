b03bc90b057e905c828555cca803dcd1b5ca389f
Mauricio Muratori <maurimura@gmail.com>
2019-11-22 16:24:21 -0300

    Validated password. Added CORS

--- src/db.rs
+++ src/db.rs
@@ -8,15 +8,9 @@ use r2d2_mongodb::{ConnectionOptions, MongodbConnectionManager };
 
 pub fn init() -> r2d2::Pool<r2d2_mongodb::MongodbConnectionManager> {
 
-    let key = "TEST_ENV";
-    let var = dotenv::var(key).unwrap();
-
-    println!("{:?}", var);
     let db_host: &str = &dotenv::var("MONGO_HOST").expect("Env variable MONGO_HOST required");
     let db_port = dotenv::var("MONGO_PORT").expect("Env variable MONGO_PORT required").parse::<u16>().unwrap();
     let db_name: &str = &dotenv::var("MONGO_DB").expect("Env variable MONGO_DB required");
-
-
 
     let manager = MongodbConnectionManager::new(
         ConnectionOptions::builder()
